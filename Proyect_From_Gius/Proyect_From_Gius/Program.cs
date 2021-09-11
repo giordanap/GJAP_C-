49bef6153e2a7ad751cb6437def4d13b31775f08
giordanap <40145046+giordanap@users.noreply.github.com>
2021-09-11 10:48:23 -0500

    finish

---
+++ Proyect_From_Gius/Proyect_From_Gius/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proyect_From_Gius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void realizarTarea()
+        {
+
+            punto origen = new punto();
+
+            punto destin = new punto(128, 88);
+        }
+
+        
+    }
+}
---
+++ Proyect_From_Gius/Proyect_From_Gius/punto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyect_From_Gius
+{
+    class punto
+    {
+
+        public punto (int x, int y)
+        {
+            Console.WriteLine($"Coordenada x: {x}, Coordenaday: {y}");
+        } 
+        public punto()
+        {
+            Console.WriteLine("este es el constructor con defecto");
+        }
+    }
+}
